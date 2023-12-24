ce8238452702e68a86181dbbb82e04b4f738a741
Shunsuke-1994 <oolongtea1980@gmail.com>
2023-12-25 00:54:23 +0900

    add shape scores

--- molcloud/main.py
+++ molcloud/main.py
@@ -28,6 +28,8 @@ def smiles(smiles_file, output_file, width, background_color, node_size, quiet, 
 @click.argument("fasta-file", type=click.Path(exists=True))
 @click.option("--output-file", default="rnacloud.png", type=click.Path(exists=False), help="Path to the output file")
 @click.option("--width", type=int, default=10, help="Width of the output image")
+@click.option("--use-shape", is_flag=True, default=False, help="Use shape information from the fasta file")
+@click.option("--cmap", type=str, default="Reds", help="Color map for the shape information")
 @click.option("--background-color", default="#f5f4e9", help="Background color of the output image")
 @click.option("--node-size", type=int, default=10, help="Size of the nodes")
 @click.option("--quiet", is_flag=True, default=False, help="Don't show progress")
@@ -44,6 +46,7 @@ def rna(fasta_file, output_file, width, background_color, node_size, quiet):
             else:
                 fasta_text += line
     plt.figure(figsize=(width, width))
-    plot_rnacloud(fasta_texts, background_color=background_color,
+    plot_rnacloud(fasta_texts, use_shape=use_shape, cmap=cmap,
+                  background_color=background_color,
                   node_size=node_size, quiet=quiet)
     plt.savefig(output_file)
